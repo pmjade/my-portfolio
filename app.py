c4c20e9c8ab0ec084abef8ed41e133373706e54a
Jade <jadetheescaper@gmail.com>
2025-06-30 19:17:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def home():
     return send_file('MyPortfolio.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
